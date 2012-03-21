594e2cf622a4dce69b418dbf9f46910865628831
Sam Burnett <srburnet@gmail.com>
2012-03-21 17:31:42 -0400

    scoping

--- bmlua/graphs.lua
+++ bmlua/graphs.lua
@@ -7,15 +7,15 @@ require('bmlua.set')
 -- dependencies. (Nodes point to their dependencies.) This returns an array of
 -- nodes in topologically sorted order, or nil if sorting is impossible.
 function topological_sort(dependencies)
-    sorted = {}
-    unsorted = bmlua.set.Set()
+    local sorted = {}
+    local unsorted = bmlua.set.Set()
     for parent in pairs(dependencies) do
         unsorted:add(parent)
     end
     while unsorted:length() > 0 do
-        has_leaf = false
+        local has_leaf = false
         for parent in unsorted:iter() do
-            has_children = false
+            local has_children = false
             for _, child in ipairs(dependencies[parent]) do
                 if unsorted:contains(child) then
                     has_children = true
